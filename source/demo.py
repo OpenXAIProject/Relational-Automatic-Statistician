3ad81fbfdf9df22a1684820607a240a0d53c658f
pandorothy <lhc101020@unist.ac.kr>
2019-07-29 11:48:33 +0900

    remove comment in demo.py and fix minor bug

--- source/demo.py
+++ source/demo.py
@@ -4,7 +4,5 @@ import postprocessing
 
 experiment.run_experiment_file('../srkl-experiments/stocks.py')
 
-# To see the outcome of this experiment, look in examples/01-airline_result.txt
-
-postprocessing.make_all_1d_figures(folder='../srkl-results/stocks/', save_folder='../srkl-report/stocks/', data_folder='../srkl-data/stocks/', rescale=False)
+postprocessing.make_all_1d_figures(folders='../srkl-results/stocks/', save_folder='../srkl-report/stocks/', data_folder='../srkl-data/stocks/', rescale=False)
     
